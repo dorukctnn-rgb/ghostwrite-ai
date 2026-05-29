f329438033ffecaf439712aed2cd6354cf9fb5c0
dorukctnn-rgb <dorukctnn@gmail.com>
2026-05-29 20:45:49 +0300

    Fix package.json main and start to server.js

---
+++ fixpkg.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+let p = JSON.parse(fs.readFileSync('package.json','utf8'));
+p.main = 'server.js';
+if(!p.scripts) p.scripts = {};
+p.scripts.start = 'node server.js';
+fs.writeFileSync('package.json', JSON.stringify(p, null, 2));
+console.log('reviewreply package.json duzeltildi');
